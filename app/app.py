c400f219040af0cba95728247232579261ea9882
Ahmed M Miqdad <ahmmij2325@gmail.com>
2026-07-11 12:42:51 +0300

    simple modify on app.py to test automation ci

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Jenkins Pipeline is fully functional!'
+    return 'Jenkins Pipeline is fully functional!, test for pipeline automation.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
